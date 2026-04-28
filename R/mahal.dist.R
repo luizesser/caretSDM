3ef192fcfdb38a87224d524c614533ed9d563e7c
Luíz Fernando Esser <luizesser@gmail.com>
2026-04-28 10:03:22 -0300

    v.1.8.3

--- R/mahal.dist.R
+++ R/mahal.dist.R
@@ -1,4 +1,4 @@
-# Custom Mahalanobis Distance model to be implemented in caret
+# Custom Mahalanobis Distance model to be implemented in caret imported from ECDFniche package
 #' @keywords internal
 #' @importFrom ECDFniche mahal.dist
 .mahal.dist <- ECDFniche::mahal.dist
